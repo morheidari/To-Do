ff993ae0b4282f74d389539609df2aa4bd7eebbd
mohammadreza <moreh412000@gmial.com>
2023-08-07 18:46:47 +0330

    add images of icons

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,18 @@
 import { format } from "date-fns";
 import "./style.css";
+import inboxSVG from "./icons/inbox.svg";
+import todaySVG from "./icons/today.svg";
+import weekSVG from "./icons/week.svg";
+import importantSVG from "./icons/important.svg";
+import projectsSVG from "./icons/folder.svg";
+import projectSVG from "./icons/project.svg";
 
 // layout
 // header
 function addElement(typeOfElement, className, content) {
   const element = document.createElement(typeOfElement);
   element.classList.add(className);
-  if (content) element.textContent = content;
+  if (content) element.innerHTML = content;
   return element;
 }
 
@@ -20,33 +26,56 @@ const main = addElement("div", "main");
 const footer = addElement("div", "footer", "©moreheidari");
 app.append(header, main, footer);
 
-const logo = addElement("div", "logo", "Todo List");
+const logo = addElement("div", "logo");
 const todayDate = addElement(
   "div",
   "today-date",
   `${format(new Date(), "eeee dd/MMM/yyyy")}`
 );
 header.append(logo, todayDate);
+const logoText = addElement("div", "logo-text", "Todo List");
+const todoIcon = addElement("div", "todo-icon");
+logo.append(logoText, todoIcon);
 
 const dashboard = addElement("div", "dashboard");
 const display = addElement("div", "display");
 main.append(dashboard, display);
 
 const boxes = addElement("div", "boxes");
-const projects = addElement("div", "projects");
+const projectBox = addElement("div", "project-box");
 const addBtn = addElement("button", "add-btn", "+");
-dashboard.append(boxes, projects, addBtn);
+dashboard.append(boxes, projectBox, addBtn);
 
 const projectTitle = addElement("div", "project-title");
 const todoBoxes = addElement("div", "todo-boxes");
 display.append(projectTitle, todoBoxes);
 
-const bInbox = addElement("div", "inbox", "inbox");
-bInbox.innerHTML = "<span class='material-icons-outlined'>inbox</span>";
-const bToday = addElement("div", "today", "today");
-const bThisweek = addElement("div", "this-week", "this week");
-const bImportant = addElement("div", "important", "important");
+const bInbox = addElement("div", "inbox", `<img src=${inboxSVG}></img> Inbox`);
+const bToday = addElement("div", "today", `<img src=${todaySVG}></img> Today`);
+const bThisweek = addElement(
+  "div",
+  "this-week",
+  `<img src=${weekSVG}></img> This week`
+);
+const bImportant = addElement(
+  "div",
+  "important",
+  `<img src=${importantSVG}></img> Important`
+);
 boxes.append(bInbox, bToday, bThisweek, bImportant);
+const projectBoxTitle = addElement(
+  "div",
+  "project-box-title",
+  `<img src=${projectsSVG}></img>Projects`
+);
+const projects = addElement("div", "projects");
+projectBox.append(projectBoxTitle, projects);
+const defaultProject = addElement(
+  "div",
+  "project",
+  `<img src=${projectSVG}></img> default project`
+);
+projects.append(defaultProject);
 
 // defining a function for displaying todos
 function displayTodoBox(todo) {
--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,10 @@ module.exports = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset/resource",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+      // {
+      //   test: /\.svg$/i,
+      //   use: ["svg-inline-loader"],
+      // },
     ],
   },
 };
